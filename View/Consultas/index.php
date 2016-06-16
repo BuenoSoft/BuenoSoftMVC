c9bd78a050365398656dbae997b44b5003fe6f4c
detectivejd <corajejd@gmail.com>
2016-06-16 01:25:52 -0300

    bug arreglado

--- View/Consultas/index.php
+++
@@ -1,8 +0,0 @@
-<h3><i class="fa fa-angle-right"></i> Consultas e Informes</h3>
-<div class="content-panel">
-    <section id="unseen" style="padding-left: 5px; padding-right: 5px;">
-        <a href="index.php?c=consultas&a=periodo">
-            <i class="fa fa-calendar"></i>&nbsp;Aplicaciones por Período
-        </a>                
-    </section>
-</div>
